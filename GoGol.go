37a8f5a0ab24080f570e2c2c2de6d36c2a6805e5
Jibble330 <jettb6@gmail.com>
2022-11-13 23:38:16 -0800

    Add neighbors (not working)

--- GoGol.go
+++ GoGol.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"path"
 	"runtime"
+	"time"
 
-	"image/color"
 	_ "image/png"
 
 	"github.com/faiface/pixel"
@@ -17,49 +17,73 @@ import (
 
 const (
     SCREENX, SCREENY = 960, 540
-    BOARDX, BOARDY = 48, 27 // resolution/20
+    BoardX, BoardY = 48, 27 // resolution/20
 )
 
 var (
     win *pixelgl.Window
     imd *imdraw.IMDraw
+    running bool
 )
 
-type board struct {
-    cells [BOARDX][BOARDY]bool
-    neighbours [BOARDX][BOARDY]uint8
+type Board struct {
+    cells [BoardX][BoardY]bool
 }
 
-func NewBoard() *board {
-    return &board{}
+func NewBoard() *Board {
+    return &Board{}
 }
 
-func (b *board) Set(x, y int, state bool) {
+func (b *Board) Set(x, y int, state bool) {
     b.cells[x][y] = state
 }
 
-func (b *board) Get(x, y int) bool {
+func (b *Board) Get(x, y int) bool {
     return b.cells[x][y]
 }
 
-func (b *board) Neighbours(x, y int) int {
-    return int(b.neighbours[x][y])
-}
-
-func (b *board) UpdateNeighbors() {
-    for x := 0; x < BOARDX; x++ {
-        for y := 0; y < BOARDY; y++ {
-            var neighbours uint8
-        }
+func (b *Board) Neighbours(x, y int) int {
+    // loop min and max if they are outside the bounds            
+    min_y := y-1
+    max_y := y+1
+    if min_y < 0 {
+        min_y = BoardY-1
+    } else if max_y >= BoardY {
+        max_y = 0
     }
+
+    min_x := x-1
+    max_x := x+1
+    if min_x < 0 {
+        min_x = BoardX-1
+    } else if max_x >= BoardX {
+        max_x = 0
+    }
+
+    // check each neighbor, incrementing count if alive
+    var neighbours int
+
+    if b.Get(min_x, min_y) {neighbours++}
+    if b.Get(x, min_y) {neighbours++}
+    if b.Get(max_x, min_y) {neighbours++}
+
+    if b.Get(min_x, y) {neighbours++}
+    if b.Get(x, y) {neighbours++}
+    if b.Get(max_x, y) {neighbours++}
+
+    if b.Get(min_x, max_y) {neighbours++}
+    if b.Get(x, max_y) {neighbours++}
+    if b.Get(max_x, y) {neighbours++}
+
+    return neighbours
 }
 
-func (b *board) Update() {
-    for x := 0; x < BOARDX; x++ {
-        for y := 0; y < BOARDY; y++ {
+func (b *Board) Update() {
+    for x := 0; x < BoardX; x++ {
+        for y := 0; y < BoardY; y++ {
             neighbours := b.Neighbours(x, y)
             if b.Get(x, y) { // check if the cell is alive
-                if neighbours > 3 || neighbours < 2 {
+                if neighbours != 2 && neighbours != 3 {
                     b.Set(x, y, false)
                 }
             } else if neighbours == 3 {
@@ -69,10 +93,10 @@ func (b *board) Update() {
     }
 }
 
-func (b *board) Draw() {
+func (b *Board) Draw() {
     imd.Color = colornames.White
-    for x := 0; x < BOARDX; x++ {
-        for y := 0; y < BOARDY; y++ {
+    for x := 0; x < BoardX; x++ {
+        for y := 0; y < BoardY; y++ {
             if b.Get(x, y) {
                 imd.Push(pixel.V(float64(x*20), float64(y*20)))
                 imd.Push(pixel.V(float64(x*20)+20, float64(y*20)+20))
@@ -115,7 +139,7 @@ func run() {
     cfg := pixelgl.WindowConfig{
         Title:     "Go Pixel",
         Bounds:    pixel.R(0, 0, SCREENX, SCREENY),
-        Maximized: true,
+        //Maximized: true,
         Icon:      []pixel.Picture{icon},
         VSync: true,
     }
@@ -125,11 +149,34 @@ func run() {
     }
 
     imd = imdraw.New(nil)
+    
+    ticker := time.NewTicker(time.Second/2)
+    board := NewBoard()
 
     for !win.Closed() {
         imd.Clear()
 
-        // run game loop here
+        if win.JustPressed(pixelgl.KeyEscape) {
+            win.SetClosed(true)
+        }
+
+        if win.JustPressed(pixelgl.KeySpace) {
+            running = !running
+        }
+
+        if running {
+            select {
+            case <-ticker.C:
+                board.Update()
+            default:
+            }
+        } else if win.JustPressed(pixelgl.MouseButtonLeft) {
+            pos := win.MousePosition()
+            cell_x, cell_y := int(pos.X/20), int(pos.Y/20)
+            board.Set(cell_x, cell_y, !board.Get(cell_x, cell_y))
+        }
+            
+        board.Draw()
 
         win.Clear(colornames.Black)
         imd.Draw(win)
